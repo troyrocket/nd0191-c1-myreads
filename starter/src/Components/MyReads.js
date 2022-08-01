4f385af0dcf8f5c5a742db90c968b31545a3d318
troyyan <yanweicheng75@163.com>
2022-08-01 18:11:41 +0800

    Building static components

---
+++ starter/src/Components/MyReads.js
@@ -0,0 +1,22 @@
+import CurrentReading from "./CurrentReading";
+import WantToRead from "./WantToRead";
+import Read from "./Read";
+
+const MyReads = () => {
+  return (
+    <div className="list-books">
+      <div className="list-books-content">
+      <div>
+            <div className="list-books-title">
+                  <h1>MyReads</h1>
+            </div>
+         <CurrentReading />
+         <WantToRead />
+         <Read />
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default MyReads
