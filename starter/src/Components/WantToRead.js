c5088c1bb1f6ce0beda1209e0ad06276d49ab41c
troyyan <yanweicheng75@163.com>
2022-08-02 18:10:57 +0800

    Add width and height details to book

--- starter/src/Components/WantToRead.js
+++ starter/src/Components/WantToRead.js
@@ -9,11 +9,16 @@ const WantToRead = () => {
                         <Book image={"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api"} 
                                 title={"1776"} 
                                 author={"David McCullough"}
+                                widthnum={128}
+                                heightnum={193}
                             />
 
                         <Book image={"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"} 
                                 title={"Harry Potter and the Sorcerer's Stone"} 
-                                author={"J.K. Rowling"}/>   
+                                author={"J.K. Rowling"}
+                                widthnum={128}
+                                heightnum={192}                           
+                                />   
                   </ol>
                 </div>
               </div>
