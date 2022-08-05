bd40856320de96c7e74b706c2743f7ee5f68f326
troyyan <yanweicheng75@163.com>
2022-08-05 14:16:40 +0800

    Navigation using Route

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,26 +3,21 @@ import { useState, useEffect } from "react";
 import MyReads from "./Components/MyReads";
 import Search from "./Components/Search";
 import * as BooksAPI from "./BooksAPI";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [showSearchPage, setShowSearchpage] = useState(false);
-
-  const callShowSearchPage = () =>  {
-          setShowSearchpage(!showSearchPage)
-  };
+  // let navigate = useNavigate();
 
   const [books, setBooks] = useState([]);
+  const [searchBooks, setSearchBooks] = useState([]); 
 
-        useEffect(() => {
-          const getBooks = async () => {
-            const res = await BooksAPI.getAll();
-            setBooks(res);
-          };
-
-          getBooks();
-        }, []);
-
-    console.log(books);
+    useEffect(() => {
+        const getBooks = async () => {
+        const res = await BooksAPI.getAll();
+        setBooks(res);
+        };
+            getBooks();
+          }, []);
 
    /* When changing a book's shelf, updating the shelf status in the API*/
    const  changeShelf= async(book, shelf) => {
@@ -32,27 +27,40 @@ function App() {
         const res = await BooksAPI.getAll();
         setBooks(res);
       };
-
       await getBooks();
    }
+ 
+   const matchBook = async (newquery) => {
+       const res = await BooksAPI.search(newquery);
+       setSearchBooks(res);
+     }
 
-   console.log(books);
+    const resetSearch = () => {
+        setSearchBooks([]); 
+      };  
 
   return (
-    <div className="app">
-      {showSearchPage ? (
-        <Search callShowSearchPage={callShowSearchPage} books = {books}/>
-        
-      ) : (
-
-         <div>
-         <MyReads books = {books} changeShelf={changeShelf}/>
-         <div className="open-search">
-            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
-          </div>
-        </div>
-      )}
-    </div>
+     <div className="app">   
+        <Routes>
+          <Route 
+              exact path="/"
+              element={
+                <MyReads books = {books} 
+                        changeShelf={changeShelf}
+                />}
+          />
+          <Route
+              exact path="/search"  
+              element={
+                <Search changeShelf={changeShelf} 
+                        books = {books} 
+                        searchBooks = {searchBooks}
+                        matchBook= {matchBook}
+                        resetSearch = {resetSearch}
+                />}    
+          />
+        </Routes>   
+      </div>  
   );
 }
 
